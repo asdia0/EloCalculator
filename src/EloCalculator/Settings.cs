c9d55ff4d01c5c5397220326b04e6b4e56588cf1
asdia <asdia.the.potato@gmail.com>
2021-06-21 12:28:11 +0800

    kinda dumb ngl

--- src/EloCalculator/Settings.cs
+++ src/EloCalculator/Settings.cs
@@ -8,6 +8,6 @@
         /// <summary>
         /// The connection string of the database.
         /// </summary>
-        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\eytan\source\repos\asdia0\EloCalculator\src\EloCalculator\Database1.mdf;Integrated Security=True";
+        public static string connectionString = @"Your-Connection-String";
     }
 }
