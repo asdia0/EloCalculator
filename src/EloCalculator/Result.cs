39cc33d69ce2d52bf31f6a974f4a83e818abb3e6
asdia0 <asdia.the.potato@gmail.com>
2021-07-03 22:56:25 +0800

    Add docs to Result

--- src/EloCalculator/Result.cs
+++ src/EloCalculator/Result.cs
@@ -1,9 +1,23 @@
 ﻿namespace EloCalculator
 {
+    /// <summary>
+    /// Types of <see cref="Game"/> results.
+    /// </summary>
     public enum Result
     {
+        /// <summary>
+        /// White wins.
+        /// </summary>
         White,
+
+        /// <summary>
+        /// Black wins.
+        /// </summary>
         Black,
+
+        /// <summary>
+        /// Game is a draw.
+        /// </summary>
         Draw,
     }
 }
