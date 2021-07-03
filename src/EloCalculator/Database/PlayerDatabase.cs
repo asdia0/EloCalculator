1cef454445866a53ab49c39ee06cc8a245f50e4d
asdia0 <asdia.the.potato@gmail.com>
2021-07-03 23:10:04 +0800

    Add docs to PlayerDatabase

--- src/EloCalculator/Database/PlayerDatabase.cs
+++ src/EloCalculator/Database/PlayerDatabase.cs
@@ -6,16 +6,30 @@
     using System.Linq;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Contains methods related to <see cref="Player"/>s.
+    /// </summary>
     public static class PlayerDatabase
     {
+        /// <summary>
+        /// <see cref="Player"/>s' initial rating.
+        /// </summary>
         public static double DefaultRating = 1000;
 
+        /// <summary>
+        /// The largest change that can occur to a <see cref="Player"/>'s rating.
+        /// </summary>
         public static int KCoefficient = 40;
 
-        public static int BenchmarkCoefficient = 400;
-
+        /// <summary>
+        /// A list of all players loaded.
+        /// </summary>
         public static List<Player> Players = new List<Player>();
 
+        /// <summary>
+        /// Load <see cref="Player"/>s from a file as a JSON object.
+        /// </summary>
+        /// <param name="path">The path to the file to load from.</param>
         public static void Load(string path)
         {
             string text = File.ReadAllText(path);
@@ -33,11 +47,19 @@
             Players.AddRange(players);
         }
 
-        public static string Export()
+        /// <summary>
+        /// Exports <see cref="Player"/> to a file as a JSON object.
+        /// </summary>
+        /// <param name="path">The path to the file to export to.</param>
+        public static void Export(string path)
         {
-            return JsonConvert.SerializeObject(Players, Formatting.Indented);
+            File.WriteAllText(path, JsonConvert.SerializeObject(Players, Formatting.Indented));
         }
 
+        /// <summary>
+        /// Updates the ratings of the <paramref name="game"/>'s players.
+        /// </summary>
+        /// <param name="game">The game to get the players to update from.</param>
         public static void UpdateRatings(Game game)
         {
             Player white = game.White;
@@ -73,15 +95,19 @@
                     }
             }
 
-            double expectedScoreA = 1 / (1 + Math.Pow(10, (blackRating - whiteRating) / BenchmarkCoefficient));
+            double expectedScoreA = 1 / (1 + Math.Pow(10, (blackRating - whiteRating) / 400));
 
-            double expectedScoreB = 1 / (1 + Math.Pow(10, (whiteRating - blackRating) / BenchmarkCoefficient));
+            double expectedScoreB = 1 / (1 + Math.Pow(10, (whiteRating - blackRating) / 400));
 
             white.Rating = whiteRating + (KCoefficient * (scoreA - expectedScoreA));
             black.Rating = blackRating + (KCoefficient * (scoreB - expectedScoreB));
         }
 
-        public static List<Player> GetRankings()
+        /// <summary>
+        /// Gets a leaderboard of all players in <see cref="Players"/> based upon <see cref="Player.Rating"/>.
+        /// </summary>
+        /// <returns>A list of <see cref="Player"/> sorted from highest rating to lowest.</returns>
+        public static List<Player> GetLeaderboard()
         {
             return Players.OrderByDescending(i => i.Rating).ToList();
         }
