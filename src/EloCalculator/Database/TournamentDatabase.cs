986f1b64caa776fd20dff7c65608d8ffd4efc6de
asdia0 <asdia.the.potato@gmail.com>
2021-07-03 23:13:06 +0800

    Add docs to TournamentDatabase

--- src/EloCalculator/Database/TournamentDatabase.cs
+++ src/EloCalculator/Database/TournamentDatabase.cs
@@ -5,10 +5,20 @@
     using System.Linq;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Contains methods related to <see cref="Tournament"/>s.
+    /// </summary>
     public class TournamentDatabase
     {
+        /// <summary>
+        /// A list of all tournaments loaded.
+        /// </summary>
         public static List<Tournament> Tournaments = new List<Tournament>();
 
+        /// <summary>
+        /// Loads a <see cref="Tournament"/>s from a file as a JSON object.
+        /// </summary>
+        /// <param name="path">The path to the file to load from.</param>
         public static void LoadTournament(string path)
         {
             string text = File.ReadAllText(path);
@@ -26,14 +36,23 @@
             Tournaments.AddRange(tournaments);
         }
 
-        public static string Export()
+        /// <summary>
+        /// Exports <see cref="Tournaments"/> to a file as a JSON object.
+        /// </summary>
+        /// <param name="path">The path to the file to export to.</param>
+        public static void Export(string path)
         {
-            return JsonConvert.SerializeObject(Tournaments, Formatting.Indented);
+            File.WriteAllText(path, JsonConvert.SerializeObject(Tournaments, Formatting.Indented));
         }
 
-        public static string Export(int id)
+        /// <summary>
+        /// Exports a <see cref="Tournament"/> to a file as a JSON object.
+        /// </summary>
+        /// <param name="path">The path to the file to export to.</param>
+        /// <param name="id">The unique identification number of the tournament to export.</param>
+        public static void Export(string path, int id)
         {
-            return JsonConvert.SerializeObject(Tournaments.Where(i => i.ID == id).FirstOrDefault(), Formatting.Indented);
+            File.WriteAllText(path, JsonConvert.SerializeObject(Tournaments.Where(i => i.ID == id).FirstOrDefault(), Formatting.Indented));
         }
     }
 }
