18199d49755d7461930ffe1c3d243d201aadad24
asdia0 <asdia.the.potato@gmail.com>
2021-07-03 23:13:20 +0800

    Add docs to Game

--- src/EloCalculator/Game.cs
+++ src/EloCalculator/Game.cs
@@ -3,14 +3,26 @@
     using System;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Represents a game.
+    /// </summary>
     public class Game
     {
+        /// <summary>
+        /// Gets the game's unique identification number.
+        /// </summary>
         [JsonProperty("ID")]
         public int ID { get; }
 
+        /// <summary>
+        /// Gets the <see cref="Player"/> that played as white.
+        /// </summary>
         [JsonIgnore]
         public Player White { get; }
 
+        /// <summary>
+        /// Gets <see cref="White"/>'s unique identification number.
+        /// </summary>
         [JsonProperty("White")]
         public int WhiteID
         {
@@ -20,9 +32,15 @@
             }
         }
 
+        /// <summary>
+        /// Gets the <see cref="Player"/> that played as black.
+        /// </summary>
         [JsonIgnore]
         public Player Black { get; }
 
+        /// <summary>
+        /// Gets <see cref="Black"/>'s unique identification number.
+        /// </summary>
         [JsonProperty("Black")]
         public int BlackID
         {
@@ -32,15 +50,32 @@
             }
         }
 
+        /// <summary>
+        /// Gets the game's <see cref="Result"/>.
+        /// </summary>
         [JsonProperty("Result")]
         public Result Result { get; }
 
+        /// <summary>
+        /// Gets the time at which the game was played.
+        /// </summary>
         [JsonProperty("DateTime")]
         public DateTime DateTime { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether the game affects <see cref="White"/> and <see cref="Black"/>'s ratings.
+        /// </summary>
         [JsonProperty("Rated")]
         public bool Rated { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Game"/> class.
+        /// </summary>
+        /// <param name="white">The <see cref="Player"/> that played as white.</param>
+        /// <param name="black">The <see cref="Player"/> that played as black.</param>
+        /// <param name="result">The game's <see cref="Result"/>.</param>
+        /// <param name="dateTime">The time at which the game was played.</param>
+        /// <param name="rated">A value indicating whether the game affects <paramref name="white"/> and <paramref name="black"/>'s ratings.</param>
         public Game(Player white, Player black, Result result, DateTime dateTime, bool rated)
         {
             this.ID = GameDatabase.Games.Count;
@@ -55,6 +90,10 @@
             PlayerDatabase.UpdateRatings(this);
         }
 
+        /// <summary>
+        /// Gets a JSON string representing the game.
+        /// </summary>
+        /// <returns>A JSON string that represents the game.</returns>
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
