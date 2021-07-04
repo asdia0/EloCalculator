17a15424300e60b94175c3739d489d98cc59b581
asdia0 <asdia.the.potato@gmail.com>
2021-07-04 18:46:23 +0800

    Add TournamentDatabaseTest

---
+++ src/EloCalculator.Test/Database/TournamentDatabaseTest.cs
@@ -0,0 +1,50 @@
+﻿namespace EloCalculator.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using EloCalculator;
+
+    [TestClass]
+    public class TournamentDatabaseTest
+    {
+        [TestMethod]
+        public void TournamentDatabaseTournaments()
+        {
+            Tournament tournament = new("Tournament 1", TournamentType.Arena);
+
+            List<Tournament> tournaments = new()
+            {
+                tournament,
+            };
+
+            CollectionAssert.AreEquivalent(tournaments, TournamentDatabase.Tournaments);
+
+            TournamentDatabase.Tournaments.Clear();
+        }
+
+        [TestMethod]
+        public void GameDatabaseLoadAndExport()
+        {
+            Tournament game = new("Tournament 1", TournamentType.Arena);
+
+            TournamentDatabase.Export("tournaments.json");
+
+            TournamentDatabase.Tournaments.Clear();
+
+            TournamentDatabase.Load("tournaments.json");
+
+            Tournament actual = TournamentDatabase.Tournaments[0];
+
+            Tournament expected = new("Tournament 1", TournamentType.Arena);
+
+            Assert.AreEqual(0, actual.ID);
+            Assert.AreEqual(expected.Name, actual.Name);
+            Assert.AreEqual(expected.Type, actual.Type);
+            CollectionAssert.AreEquivalent(expected.Players, actual.Players);
+            CollectionAssert.AreEquivalent(expected.Rounds, actual.Rounds);
+
+            TournamentDatabase.Tournaments.Clear();
+        }
+    }
+}
